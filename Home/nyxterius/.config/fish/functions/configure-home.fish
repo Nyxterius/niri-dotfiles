1f0c77a1be9b345accddb64804a9ac3cd2f0113c
Nyxterius <77221598+Nyxterius@users.noreply.github.com>
2026-01-31 11:32:31 -0500

    Rebuilt

--- Home/nyxterius/.config/fish/functions/configure-home.fish
+++
@@ -1,3 +0,0 @@
-function configure-home --wraps='sudo edit ~/.config/home-manager/home.nix' --description 'alias configure-home sudo edit ~/.config/home-manager/home.nix'
-    sudo edit ~/.config/home-manager/home.nix $argv
-end
